aa2c76dd29b97036f6254d77e549faca03d0e385
Alexander Golden <alexander.golden@fmr.com>
2021-12-31 15:06:05 -0500

    Add barebones Swift package

---
+++ Sources/HomeAssistantSDK/HomeAssistantSDK.swift
@@ -0,0 +1,6 @@
+public struct HomeAssistantSDK {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/HomeAssistantSDKTests/HomeAssistantSDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HomeAssistantSDK
+
+final class HomeAssistantSDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HomeAssistantSDK().text, "Hello, World!")
+    }
+}
